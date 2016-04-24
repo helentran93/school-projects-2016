20d2308a99492f91a9e4ae04c77b778846504a17
helentran93 <tranhelen93@gmail.com>
2016-04-24 19:16:30 +0300

    Updated Seinä
    Added new methods such as getters and setters for walls' coordinates and descriptions of each method and instance variable.

--- Seina.java
+++ Seina.java
@@ -1,44 +1,105 @@
 package mvc;
 
-public class Sein� {
+/**
+ * Luokka, joka sisältää tiedot seinän koordinaateista.
+ *
+ * @author Helen
+ * @version Java 8
+ *
+ */
+public class Seinä {
 
-	float x, y, x2, y2;
+	/**
+	 * Seinän koordinaatin x-arvo.
+	 */
+	private float x;
+	/**
+	 * Seinän koordinaatin y-arvo.
+	 */
+	private float y;
+	/**
+	 * Seinän koordinaatin x2-arvo/leveyden arvo.
+	 */
+	private float x2;
+	/**
+	 * Seinän koordinaatin y2-arvo/korkeuden arvo.
+	 */
+	private float y2;
 
-	public Sein�(float x, float y, float x2, float y2) {
+	/**
+	 * Seinä-luokan parametrillinen konstruktori.
+	 * @param x koordinaatin x-arvo
+	 * @param y koordinaatin y-arvo
+	 * @param x2 koordinaatin x2-arvo
+	 * @param y2 koordinaatin y2-arvo
+	 */
+	public Seinä(float x, float y, float x2, float y2) {
 		this.x = x;
 		this.y = y;
 		this.x2 = x2;
 		this.y2 = y2;
 	}
 
+	/**
+	 * Setteri x-arvolle.
+	 * @param x koordinaatin x-arvo
+	 */
 	public void setX(float x){
 		this.x = x;
 	}
 
+	/**
+	 * Setteri y-arvolle.
+	 * @param y koordinaatin y-arvo
+	 */
 	public void setY(float y){
 		this.y = y;
 	}
 
+	/**
+	 * Setteri x2-arvolle.
+	 * @param x2 koordinaatin x2-arvo
+	 */
 	public void setX2(float x2){
 		this.x2 = x2;
 	}
 
+	/**
+	 * Setteri y2-arvolle.
+	 * @param y2 koordinaatin y2-arvo
+	 */
 	public void setY2(float y2){
 		this.y2 = y2;
 	}
 
+	/**
+	 * Getteri x-arvolle.
+	 * @return Palauttaa x-arvon.
+	 */
 	public float getX(){
 		return this.x;
 	}
 
+	/**
+	 * Getteri y-arvolle.
+	 * @return Palauttaa y-arvon.
+	 */
 	public float getY(){
 		return this.y;
 	}
 
+	/**
+	 * Getteri x2-arvolle.
+	 * @return Palauttaa x2-arvon.
+	 */
 	public float getX2(){
 		return this.x2;
 	}
 
+	/**
+	 * Getteri y2-arvolle.
+	 * @return Palauttaa y2-arvon.
+	 */
 	public float getY2(){
 		return this.y2;
 	}
